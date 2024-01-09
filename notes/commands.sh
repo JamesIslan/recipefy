b1ff0e0c664a7c829f93d3965d666978eb5a2fdb
Joamerson Islan <69359116+JamesIslan@users.noreply.github.com>
2024-01-09 20:38:56 -0300

    Delete notes directory

--- notes/commands.sh
+++
@@ -1,5 +0,0 @@
-django-admin --version # Checa a versão do django
-django-admin --help # Lista de comandos django
-django-admin startproject project_name . # Inicia um projeto
-python manage.py runserver # Inicia o servidor web do projeto
-python manage.py startapp app_name
