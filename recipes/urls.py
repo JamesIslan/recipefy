c7f41cd78cf05bef60d2c277404e31572f551218
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-12 15:25:17 -0300

    feat: added the detailed view of a recipe page on `recipe-view.html`

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from recipes.views import home
+from recipes import views
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/', views.recipe),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,10 @@
+# from django.http import HttpResponse  # Temporary
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
+    # return HttpResponse(f'Essa é a receita {id}')
