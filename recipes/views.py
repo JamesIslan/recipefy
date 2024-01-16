6f7d49055756381873b277e9cf3ea227c6f29395
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-16 13:53:25 -0300

    feat: add category view for each category registered on the app database

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,29 @@
 # from django.http import HttpResponse  # Temporary
 from django.shortcuts import render
 
+from recipes.models import Category, Recipe
 from utils.recipes.dummy import make_recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(
         request, 
         'recipes/pages/home.html', 
         context={
-            'recipes': [make_recipe() for i in range(10)],
+            'recipes': recipes,
+        }
+        )
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True)
+    print(recipes) # Remove later
+    return render(
+        request, 
+        'recipes/pages/category.html', 
+        context={
+            'recipes': recipes,
+            'page_title': f'{recipes.first().category.name} - Category | '
         }
         )
 
