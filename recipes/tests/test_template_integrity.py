20415fcfad6bed0723b01d55d9629aa20c001d71
Joamerson Islan <islan.j@escolar.ifrn.edu.br>
2024-01-23 20:24:43 -0300

    test: add test for character escaping ensurance on search view

---
+++ recipes/tests/test_template_integrity.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TemplateIntegrityTest(TestCase):
+    def test_search_template_escapes_code_injection(self):
+        url = reverse('recipes:search') + '?q=<Teste>'
+        escaped_query_string = '&quot;&lt;Teste&gt;&quot;'
+        content = self.client.get(url).content.decode('utf-8')
+        self.assertIn(escaped_query_string, content)
